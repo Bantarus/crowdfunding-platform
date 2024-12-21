dbc74d52920cd12d7bacb1b8fb9ecdc6204df7e5
Bantarus <bantarusgaming@gmail.com>
2024-12-22 00:03:03 +0100

    master contract update

--- src/contracts/master.exs
+++ src/contracts/master.exs
@@ -9,18 +9,22 @@
 condition triggered_by: transaction, on: add_task(task_creation_address,title, description, goal_amount, deadline, category), as: [
   content: (
  
+    previous_address = Chain.get_previous_address(transaction)
+    creator_genesis_address = Chain.get_genesis_address(previous_address)
+
+    valid_creator? = Contract.call_function(task_creation_address, "get_creator", []) == creator_genesis_address
+
     task_creation_address = String.to_hex(task_creation_address)
     task_transaction = Chain.get_transaction(task_creation_address)
     valid_task? = task_transaction != nil
-    valid_content? = Json.parse(task_transaction.content) == Json.parse(get_task_content(title, description, goal_amount, deadline, category))
-  
+   
     valid_code? = false
-    valid_code? = Code.is_same?(get_task_code(), task_transaction.code)
-    
+    valid_code? = Code.is_same?(get_task_code(creator_genesis_address), task_transaction.code)
+
     valid_params? = goal_amount > 0 && deadline > Time.now()
 
    valid_task? &&
-   valid_content? && 
+   valid_creator? &&
    valid_code? && 
    valid_params?
     
@@ -51,9 +55,11 @@ actions triggered_by: transaction, on: add_task(task_creation_address,title, des
     nb_contributions: 0,
     min_votes: 1,
     nb_approvals: 0,
+    approve_addresses: [],
     nb_refusals: 0,
     nb_promotes: 0,
-    promote_addresses: []
+    promote_addresses: [],
+    nb_validations: 0
   ]
 
   # Store task
@@ -61,7 +67,9 @@ actions triggered_by: transaction, on: add_task(task_creation_address,title, des
   tasks = Map.set(tasks, task.id, task)
   State.set("tasks", tasks)
 
- Contract.set_content(get_task_content(title, description, goal_amount, deadline, category))
+  Contract.set_type("transfer")
+  Contract.add_recipient(address: task_creation_address, action: "init", args: [task_creation_address, title, description, goal_amount, deadline, category])
+ 
 end
 
 condition triggered_by: transaction, on: approve_task(task_creation_address), as: [
@@ -72,6 +80,8 @@ condition triggered_by: transaction, on: approve_task(task_creation_address), as
 
      valid_approver? = List.in?(get_quorum_list(),approver_genesis_address)
 
+    
+
     valid_task_address? = false
 
     if valid_approver? do
@@ -79,7 +89,8 @@ condition triggered_by: transaction, on: approve_task(task_creation_address), as
       tasks = State.get("tasks", Map.new())
       task = Map.get(tasks,String.to_hex(task_creation_address))
 
-      valid_task_address? = task != nil && task.status == "pending"
+      valid_task_address? = task != nil && task.status == "pending" && !List.in?(task.approve_addresses,approver_genesis_address)
+
 
     end
 
@@ -89,20 +100,30 @@ condition triggered_by: transaction, on: approve_task(task_creation_address), as
 ]
 
 actions triggered_by: transaction, on: approve_task(task_creation_address) do
-
+approver_previous_address = Chain.get_previous_address(transaction)
+approver_genesis_address = Chain.get_genesis_address(approver_previous_address)
+task_creation_address = String.to_hex(task_creation_address)
 tasks = State.get("tasks")
-task = Map.get(tasks, String.to_hex(task_creation_address))
+task = Map.get(tasks, task_creation_address)
 
 task = Map.set(task, "nb_approvals",task.nb_approvals + 1 )
+task = Map.set(task, "approve_addresses", List.prepend(task.approve_addresses, approver_genesis_address))
+
 
 if task_is_approved(task.nb_approvals) do
 
 task = Map.set(task,"status","active")
 
+Contract.set_type("transfer")
+Contract.add_recipient(address: task_creation_address, action: "update_status", args: ["active"])
+
+
 end
 
 tasks = Map.set(tasks,task.id, task)
 State.set("tasks",tasks)
+
+
 end
 
 condition triggered_by: transaction, on: reject_task(), as: [
@@ -116,7 +137,7 @@ end
 
 
 
-# Vote condition
+# promote condition
 condition triggered_by: transaction, on: promote_task(task_creation_address), as: [
   content: (
     valid? = false
@@ -140,7 +161,7 @@ condition triggered_by: transaction, on: promote_task(task_creation_address), as
   )
 ]
 
-# Vote action
+# promote action
 actions triggered_by: transaction, on: promote_task(task_creation_address) do
 
   previous_promote_address = Chain.get_previous_address(transaction)
@@ -166,10 +187,10 @@ end
 
 
 # Contribute condition
-condition triggered_by: transaction, on: fund_task(task_id), as: [
+condition triggered_by: transaction, on: fund_task(task_creation_address), as: [
   content: (
     tasks = State.get("tasks")
-    task = Map.get(tasks, task_id)
+    task = Map.get(tasks, task_creation_address)
     transfered_amount = Map.get(transaction.uco_transfers, Chain.get_genesis_address(contract.address))
 
     # Validate task exists and is active
@@ -183,9 +204,9 @@ condition triggered_by: transaction, on: fund_task(task_id), as: [
 ]
 
 # Contribute action
-actions triggered_by: transaction, on: fund_task(task_id) do
+actions triggered_by: transaction, on: fund_task(task_creation_address) do
   tasks = State.get("tasks")
-  task = Map.get(tasks, String.to_hex(task_id))
+  task = Map.get(tasks, String.to_hex(task_creation_address))
   
   # Get contribution amount
   uco_amount = Map.get(transaction.uco_transfers, Chain.get_genesis_address(contract.address))
@@ -193,6 +214,8 @@ actions triggered_by: transaction, on: fund_task(task_id) do
   contributor_genesis_address = Chain.get_genesis_address(contributor_previous_address)
 
   # Update task amount
+  Contract.set_type("transfer")
+  Contract.add_uco_transfer(to: task_creation_address,amount:  uco_amount)
   task = Map.set(task, "current_amount", task.current_amount + uco_amount)
 
   task = Map.set(task,"nb_contributions",task.nb_contributions + 1)
@@ -200,15 +223,64 @@ actions triggered_by: transaction, on: fund_task(task_id) do
   # Update status if funded
   if task.current_amount >= task.goal_amount do
     task = Map.set(task, "status", "funded")
+    Contract.set_type("transfer")
+    Contract.add_recipient(address: task_creation_address, action: "update_status", args: ["funded"])
+
   end
   
   # Store updated task
-  tasks = Map.set(tasks, task_id, task)
+  tasks = Map.set(tasks, task.id, task)
   State.set("tasks", tasks)
 
  
 end
 
+#validate condition
+condition triggered_by: transaction, on: validate_task(task_creation_address), as: [
+  content: (
+
+    approver_previous_address = Chain.get_previous_address(transaction)
+    approver_genesis_address = Chain.get_genesis_address(approver_previous_address)
+
+     valid_approver? = List.in?(get_quorum_list(),approver_genesis_address)
+
+    valid_task_address? = false
+
+    if valid_approver? do
+
+      tasks = State.get("tasks", Map.new())
+      task = Map.get(tasks,String.to_hex(task_creation_address))
+
+      valid_task_address? = task != nil && task.status == "funded"
+
+    end
+
+    valid_task_address?
+
+  )
+]
+
+#validate action
+actions triggered_by: transaction, on: validate_task(task_creation_address) do
+
+tasks = State.get("tasks")
+task = Map.get(tasks, String.to_hex(task_creation_address))
+
+task = Map.set(task, "nb_validations",task.nb_validations + 1 )
+
+if task_is_validated(task.nb_validations) do
+
+  task = Map.set(task,"status","completed")
+  # update task sc status 
+  Contract.set_type("transfer")
+  Contract.add_recipient(address: task_creation_address, action: "update_status", args: ["completed"])
+
+
+end
+
+tasks = Map.set(tasks,task.id, task)
+State.set("tasks",tasks)
+end
 
 
 fun get_task_content(title, description, goal_amount, deadline, category) do
@@ -229,94 +301,115 @@ fun get_task_content(title, description, goal_amount, deadline, category) do
   
 end
 
-fun get_task_code() do
+fun get_task_code(creator_genesis_address) do
 
 """
 @version 1
 
 # update status condition
-condition triggered_by: transaction, on: update_status(status), as: [
+condition triggered_by: transaction, on: init(creation_address, title, description, goal_amount, deadline, category), as: [
   content: (
 
-    # can only be updated by master
-    previous_address = Chain.get_previous_address()
-    Chain.get_genesis_address(previous_address) == 0x00004035822cdcd26e571ab7bfbd3939d967128e58be4c2cedfa747394b26c5986ba
+    # can only be init by master
+    previous_address = Chain.get_previous_address(transaction)
+    genesis_address = Chain.get_genesis_address(previous_address)
+    task = State.get("task")
+    task == nil && genesis_address == 0x0000ec63e23dc8b2dd002bcfd96c9fea1e83d9443b7f73d862455fb3ed855766f0df
+
 
     
   )
 ]
 
 # update status action
-actions triggered_by: transaction, on: update_status(status) do
-  task = State.get("task", Map.new())
-
-  # if first sc call init the state with genesis address content
-  if task == nil do 
-
-  previous_address = Chain.get_previous_address(transaction)
-  genesis_address = Chain.get_genesis_address(previous_address) 
-  genesis_transaction = Chain.get_transaction(genesis_address)
-  content = Json.parse(genesis_transaction.content)
+actions triggered_by: transaction, on: init(creation_address, title, description, goal_amount, deadline, category) do
+  
+  # init the state with creation address content
+ 
+  genesis_address = Chain.get_genesis_address(creation_address) 
+  creation_transaction = Chain.get_transaction(creation_address)
 
   task = [
-    id: genesis_address,
-    title: content.title,
-    description: content.description,
-    goal_amount: content.goalAmount,
+    id: String.to_hex(creation_address),
+    title: title,
+    description: description,
+    goal_amount: goal_amount,
     current_amount: 0,
-    deadline: content.deadline,
-    category: content.category,
-    creator: content.creator,
+    deadline: deadline,
+    category: category,
+    creator: get_creator(),
     status: "pending",
-    #created_at: content.created_at
-    creator_reliability: 0
+    created_at: creation_transaction.timestamp
 
   ]
 
-  end
-
-task = Map.set(task,"status", status)
-
+ 
 State.set("task", task)
  
 end
 
-# Contribute condition
-condition triggered_by: transaction, on: deposit(), as: [
+# update_status condition
+condition triggered_by: transaction, on: update_status(new_status), as: [
   content: (
-    task = State.get("task")
-    transfered_amount = Map.get(transaction.uco_movements, contract.address)
+    
 
-    task.status == "active" &&
-    Time.now() <= task.deadline &&
-    transfered_amount != nil && 
-    transfered_amount > 0
+    # can only be init by master and have to init first
+    previous_address = Chain.get_previous_address(transaction)
+    genesis_address = Chain.get_genesis_address(previous_address)
+    task = State.get("task")
+    task != nil && task.status != new_status && genesis_address == 0x0000ec63e23dc8b2dd002bcfd96c9fea1e83d9443b7f73d862455fb3ed855766f0df
+
+
   )
 ]
 
-# Contribute action
-actions triggered_by: transaction, on: deposit() do
+# update status action
+actions triggered_by: transaction, on: update_status(new_status) do
+
   task = State.get("task")
-  
-  # Get contribution amount
-  uco_amount = Map.get(transaction.uco_movements, contract.address)
-  contributor = Chain.get_genesis_address(transaction.address)
-
-  # Update task amount
-  task = Map.set(task, "current_amount", task.current_amount + uco_amount)
-  
-  # Update status if funded
-  if task.current_amount >= task.goal_amount do
-    task = Map.set(task, "status", "funded")
-  end
-  
+  task = Map.set(task,"status", new_status)
   State.set("task", task)
+  
+end
 
-  # Track contribution
-  contributions = State.get("contributions", Map.new())
-  contributor_amount = Map.get(contributions, contributor, 0)
-  contributions = Map.set(contributions, contributor, contributor_amount + uco_amount)
-  State.set("contributions", contributions)
+
+#withdraw condition
+condition triggered_by: transaction, on: withdraw(), as: [
+  content: (
+    valid? = false
+
+    previous_address = Chain.get_previous_address(transaction)
+    genesis_address = Chain.get_genesis_address(previous_address)
+    task = State.get("task")
+
+    valid? = task != nil && task.status == "completed" && genesis_address == String.to_hex(task.creator)
+
+
+    if valid? do
+
+      
+      valid? =  Chain.get_uco_balance(contract.address) > 0
+
+
+    end
+
+    valid?
+    
+    
+  )
+]
+
+# withdraw action
+actions triggered_by: transaction, on: withdraw() do
+
+  
+  task = State.get("task")
+
+  Contract.set_type("transfer")
+  Contract.set_content("withdraw task funds")
+  Contract.add_uco_transfer(to: transaction.address, amount: Chain.get_uco_balance(contract.address))
+  
+
 end
 
 fun init_task() do
@@ -324,6 +417,10 @@ fun init_task() do
 end
 
 # Export functions
+export fun get_creator() do
+  0x#{creator_genesis_address}
+end
+
 export fun get_task() do
   State.get("task")
 end
@@ -334,6 +431,12 @@ end
 fun task_is_approved(nb_approvals) do
 
  nb_approvals > (List.size(get_quorum_list()) / 2)
+
+end
+
+fun task_is_validated(nb_validations) do
+
+  nb_validations > (List.size(get_quorum_list()) / 2)
 
 end
 
